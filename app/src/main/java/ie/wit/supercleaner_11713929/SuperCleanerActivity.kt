785b468f8a61e47bb985d615a62c796932d204fc
ahsa91 <ahsa91@gmail.com>
2022-01-31 02:52:09 +0000

    #added timber for logging #changed MainActivity.kt to SuperCleanerActivity.kt with all neccessary dependancies

--- app/src/main/java/ie/wit/supercleaner_11713929/SuperCleanerActivity.kt
+++ app/src/main/java/ie/wit/supercleaner_11713929/SuperCleanerActivity.kt
@@ -2,10 +2,16 @@ package ie.wit.supercleaner_11713929
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
-class MainActivity : AppCompatActivity() {
+class SuperCleanerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timber.plant(Timber.DebugTree())
+
+        i("SuperCleaner Activity started..")
     }
 }
